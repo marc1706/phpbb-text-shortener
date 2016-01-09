6aa7b934fb4d000d700f422c15b59a1d6b95a519
Marc Alexander <admin@m-a-styles.de>
2016-01-09 11:10:26 +0100

    Add basic text shortening and first tests

--- src/Shortener.php
+++ src/Shortener.php
@@ -16,6 +16,9 @@ class Shortener
 	/** @var string Text */
 	private $text;
 
+	/** @var array Text splitted using preg_split() */
+	private $splitText;
+
 	/** @var int Text length */
 	private $textLength = 0;
 
@@ -24,6 +27,9 @@ class Shortener
 
 	/** @var string Shortened text */
 	private $shortenedText;
+
+	/** @var array Delimiters of current text */
+	private $delimiter;
 
 	/** @todo: Needed? */
 	public function __construct()
@@ -40,10 +46,18 @@ class Shortener
 	 */
 	public function setText($text)
 	{
-		$this->text = $text;
-		$this->textLength = strlen($text);
+		// Remove surrounding text
+		preg_match('@^(<[rt][ >])((.|\s)+)(<\/[rt][ >])$@', $text, $matches);
+		$this->text = $matches[2];
+		$this->delimiter = array($matches[1], $matches[4]);
+		$this->textLength = $this->getRealLength($text);
 
-		$this->inputValid = !empty($this->text) && $this->textLength !== 0;
+		$this->inputValid = !empty($this->text) && $this->textLength !== 0 && preg_match('/^<[rt][ >]/', $text);
+
+		if ($this->inputValid)
+		{
+			$this->splitText($this->text);
+		}
 
 		return $this;
 	}
@@ -61,6 +75,14 @@ class Shortener
 			return '';
 		}
 
+		if ($length >= $this->textLength) {
+			$this->shortenedText = $this->text;
+		} else {
+			$this->buildShortenedText($length);
+		}
+
+		$this->addDelimiters();
+
 		return $this->shortenedText;
 	}
 
@@ -74,5 +96,47 @@ class Shortener
 	protected function getRealLength($string)
 	{
 		return strlen(preg_replace('@\[([a-zA-Z0-9-_]+)\].+\[/\1\]@i', '', strip_tags($string)));
+	}
+
+	/**
+	 * Split text into parts with position info
+	 *
+	 * @param string $text Text to split
+	 */
+	protected function splitText($text)
+	{
+		$this->splitText = preg_split('@([<\[]\/?[a-zA-Z0-9]+[\]>])@i', $text, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_OFFSET_CAPTURE);
+	}
+
+	/**
+	 * Add delimiters to shortened text
+	 */
+	protected function addDelimiters()
+	{
+		$this->shortenedText = $this->delimiter[0] . $this->shortenedText  . ' ...' . $this->delimiter[1];
+	}
+
+	protected function buildShortenedText($targetLength)
+	{
+		$inSmiley = false;
+		$length = 0;
+		$this->shortenedText = '';
+
+		foreach ($this->splitText as $part) {
+			$curLength = $this->getRealLength($part[0]);
+
+			if (($curLength + $length) > $targetLength) {
+				$this->shortenedText .= substr($part[0], 0, $targetLength - $length);
+			} else {
+				$this->shortenedText .= $part[0];
+			}
+			$length = $this->getRealLength($this->shortenedText);
+
+
+
+			if ($length >= $targetLength) {
+				break;
+			}
+		}
 	}
 }
---
+++ tests/ShortenerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Text shortener for phpBB 3.2.x base class
+ * @package phpbb-text-shortener
+ * @copyright (c) Marc Alexander <admin@m-a-styles.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Marc1706\TextShortener\tests;
+
+class ShortenerTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var \Marc1706\TextShortener\Shortener */
+	protected $shortener;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->shortener = new \Marc1706\TextShortener\Shortener();
+	}
+
+	public function dataShorten()
+	{
+		return array(
+			array('<r>This is an example post in your phpBB3 installation. Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first category and your first forum are assigned an appropriate set of permissions for the predefined usergroups admini<I><s>[i]</s>strators, bots, global moderators, guests, registered users and registered COPPA users. If you also choose to delete your first category and your first forum, do not <e>[/i]</e></I>forget to assign permissions <B><s>[b]</s>for all these<e>[/b]</e></B> usergroups for all new categories and forums you create. It is recommended to rename your first category and your first forum and copy permissions from these while creating new categories and forums. Have fun!<br/>
+<IMG src="https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401"><s>[img]</s><URL url="https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401">https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401</URL><e>[/img]</e></IMG><br/>
+<br/>
+This is an example post in your phpBB3 installation.  <E>:D</E>  Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first category and your first forum are assigned an appropriate set of permissions for the predefined usergroups administrators, bots, global moderators, guests, registered users and registered COPPA users. If you also choose to delete your first category and your first forum, do not forget to assign permissions for all these usergroups for all new categories and forums you create. It is recommended to rename your first category and your first forum and copy permissions from these while creating new categories and forums. Have fun!</r>', 300, '<r>This is an example post in your phpBB3 installation. Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first category and your first forum are assigned an appropriate set of permissions for the predefined user ...</r>'),
+			array('<r>This is an example post in your phpBB3 installation. Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first category and your first forum are assigned an appropriate set of permissions for the predefined usergroups admini<I><s>[i]</s>strators, bots, global moderators, guests, registered users and registered COPPA users. If you also choose to delete your first category and your first forum, do not <e>[/i]</e></I>forget to assign permissions <B><s>[b]</s>for all these<e>[/b]</e></B> usergroups for all new categories and forums you create. It is recommended to rename your first category and your first forum and copy permissions from these while creating new categories and forums. Have fun!<br/>
+<IMG src="https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401"><s>[img]</s><URL url="https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401">https://www.phpbb.com/assets/images/images/bg_forumatic_front_page.png?v2015122401</URL><e>[/img]</e></IMG><br/>
+<br/>
+This is an example post in your phpBB3 installation.  <E>:D</E>  Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first category and your first forum are assigned an appropriate set of permissions for the predefined usergroups administrators, bots, global moderators, guests, registered users and registered COPPA users. If you also choose to delete your first category and your first forum, do not forget to assign permissions for all these usergroups for all new categories and forums you create. It is recommended to rename your first category and your first forum and copy permissions from these while creating new categories and forums. Have fun!</r>', 200, '<r>This is an example post in your phpBB3 installation. Everything seems to be working. You may delete this post if you like and continue to set up your board. During the installation process your first  ...</r>'),
+		);
+	}
+
+	/**
+	 * @dataProvider dataShorten
+	 */
+	public function testShorten($input, $length, $expected)
+	{
+		$this->assertEquals($expected, $this->shortener->setText($input)
+			->shortenText($length));
+	}
+}
